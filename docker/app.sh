8335720cf85c73a4a44a5b7b2e4a0d6f6b6fb59b
Strong-Finger-sudo <80513758+Strong-Finger-sudo@users.noreply.github.com>
2023-10-16 21:13:28 +0300

    new ip addres

--- docker/app.sh
+++ docker/app.sh
@@ -4,4 +4,4 @@ alembic upgrade head
 
 cd src
 
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=127.0.0.1:8000
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=192.168.138.132:8000
